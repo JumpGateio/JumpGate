df9ae3b7000ecc78bb4fc863bdc705bf6080da98
stygiansabyss <stygian.warlock.v2@gmail.com>
2016-08-13 21:58:56 -0500

    Fixing route file

--- routes/web/home.php
+++ routes/web/home.php
@@ -1,13 +1,7 @@
 <?php
 
-Route::group(['prefix' => 'admin'], function () {
-    Route::group(['prefix' => 'home'], function () {
-        Route::group(['prefix' => 'dashboard'], function () {
-            Route::get('/', [
-                'as'         => 'home',
-                'uses'       => 'HomeController@index',
-                'middleware' => 'active:home',
-            ]);
-        });
-    });
-});
+Route::get('/', [
+    'as'         => 'home',
+    'uses'       => 'HomeController@index',
+    'middleware' => 'active:home',
+]);
