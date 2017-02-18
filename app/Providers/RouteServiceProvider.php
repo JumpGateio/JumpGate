85471d573893d0d02dd9a7681977bb1d47f72042
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-02-18 15:02:05 -0600

    Fixing route SP

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
