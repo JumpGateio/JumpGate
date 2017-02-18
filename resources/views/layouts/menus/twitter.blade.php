1bcd553950e3f06c15383b7c51d80a513c1b1d50
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-02-18 15:19:34 -0600

    Fixing navbar for bs4

--- resources/views/layouts/menus/twitter.blade.php
+++ resources/views/layouts/menus/twitter.blade.php
@@ -9,26 +9,15 @@
   @endif
   <div class="collapse navbar-collapse" id="navbarResponsive">
     @if (Menu::exists('leftMenu') && Menu::hasLinks('leftMenu'))
-      <ul class="navbar-nav">
+      <ul class="navbar-nav mr-auto">
         @each('layouts.menus.twitter.menu', Menu::render('leftMenu')->links, 'item')
       </ul>
     @endif
-    <div class="hidden-md hidden-lg">
-      @if (Menu::exists('rightMenu') && Menu::hasLinks('rightMenu'))
-        <ul class="navbar-nav pull-xs-right">
-          @each('layouts.menus.twitter.right-menu', Menu::render('rightMenu')->links, 'item')
-        </ul>
-      @endif
-    </div>
-    <div class="hidden-sm hidden-xs">
-      <div class="container-fluid">
-        @if (Menu::exists('rightMenu') && Menu::hasLinks('rightMenu'))
-          <ul class="nav navbar-nav pull-xs-right">
-            @each('layouts.menus.twitter.right-menu', Menu::render('rightMenu')->links, 'item')
-          </ul>
-        @endif
-      </div>
-    </div>
+    @if (Menu::exists('rightMenu') && Menu::hasLinks('rightMenu'))
+      <ul class="navbar-nav">
+        @each('layouts.menus.twitter.right-menu', Menu::render('rightMenu')->links, 'item')
+      </ul>
+    @endif
   </div>
 </nav>
-<br style="clear: both;"/>
+<br style="clear: both;" />
