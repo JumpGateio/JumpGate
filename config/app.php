0ab1cd91b1b0f0cc60afdafb990517e63a6e868a
Travis Blasingame <stygian.warlock.v2@gmail.com>
2018-10-24 15:26:08 -0500

    Adding telescope

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\ComposerServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
         App\Providers\MacroServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
