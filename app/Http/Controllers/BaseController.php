79d115fd0543847e9be0fa9a518e0e0210fe804c
Travis Blasingame <stygian.warlock.v2@gmail.com>
2021-06-21 12:25:05 -0500

    Update BaseController.php

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -6,6 +6,7 @@ use App\Traits\UsesInertia;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Inertia\Inertia;
 use JumpGate\ViewResolution\Traits\AutoResolvesViews;
 use JumpGate\Core\Http\Controllers\BaseController as CoreBaseController;
 
