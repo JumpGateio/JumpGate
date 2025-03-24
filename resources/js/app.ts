54685fcb1fe9bcdb7f4f296abf78e70dd210ae47
Stygiansabyss <stygian.warlock.v2@gmail.com>
2025-03-23 22:52:01 -0500

    Removing built css from repo

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,5 +1,3 @@
-import '../css/app.css';
-
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
