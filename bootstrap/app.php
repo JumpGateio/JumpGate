710fe819f4664a5e3331d933d47a2afad1e9ee15
Stygiansabyss <stygian.warlock.v2@gmail.com>
2025-03-24 00:51:11 -0500

    Minor clean up

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use JumpGate\Menu\Middleware\MenuMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -22,7 +23,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->alias([
-            'active' => null,
+            'active' => MenuMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
