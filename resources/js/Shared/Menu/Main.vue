cac48c27c17aa80c15358cf3959c2f04a9886e27
Stygiansabyss <stygian.warlock.v2@gmail.com>
2025-03-29 16:21:17 -0500

    Making pages work on mobile

--- resources/js/Shared/Menu/Main.vue
+++ resources/js/Shared/Menu/Main.vue
@@ -8,9 +8,8 @@
           </span>
           JumpGate
         </Link>
-        <button class="navbar-toggler navbar-toggler-right hidden-md-up" type="button" data-toggle="collapse"
-                data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-                aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
--- resources/js/pages/Auth/Login.vue
+++ resources/js/pages/Auth/Login.vue
@@ -1,34 +1,28 @@
 <template>
   <div class="container-fluid vh-100 mt-5">
-    <div class="row">
-      <div class="offset-lg-3 col-lg-6">
-        <div class="card">
-          <form @submit.prevent="submit">
-            <div class="card-header">Login</div>
-            <div class="card-body pb-0">
-              <div class="row mb-3">
-                <label for="email" class="col-3 col-form-label">Email</label>
-                <div class="col-9">
-                  <input type="text" id="email" class="form-control" v-model="form.email" required>
-                  <small class="form-text text-danger" v-if="form.errors.email">{{ form.errors.email }}</small>
-                </div>
-              </div>
-              <div class="row mb-3">
-                <label for="password" class="col-3 col-form-label">Password</label>
-                <div class="col-9">
-                  <input type="password" id="password" class="form-control" v-model="form.password" required>
-                  <small class="form-text text-danger" v-if="form.errors.password">{{ form.errors.password }}</small>
-                </div>
-              </div>
-            </div>
-            <div class="card-footer">
-              <input type="submit" :disabled="form.processing" value="Login" class="btn btn-primary">
-              <Link :href="route('auth.register')" class="btn btn-link" v-if="route().has('auth.register')">
-                Register
-              </Link>
-            </div>
-          </form>
-        </div>
+    <div class="d-flex flex-column flex-md-row  gap-5 justify-content-center align-items-center">
+      <div class="flex">
+        <img src="https://place-hold.it/300x300" alt="Site Logo">
+      </div>
+      <div class="vr d-none d-sm-block" style="height: 400px;"></div>
+      <div class="flex">
+        <form @submit.prevent="submit">
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="text" id="email" class="form-control" v-model="form.email" required>
+            <small class="form-text text-danger" v-if="form.errors.email">{{ form.errors.email }}</small>
+            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Password</label>
+            <input type="password" id="password" class="form-control" v-model="form.password" required>
+            <small class="form-text text-danger" v-if="form.errors.password">{{ form.errors.password }}</small>
+          </div>
+          <input type="submit" :disabled="form.processing" value="Login" class="btn btn-primary">
+          <Link :href="route('auth.register')" class="btn btn-link" v-if="route().has('auth.register')">
+            Register
+          </Link>
+        </form>
       </div>
     </div>
   </div>
