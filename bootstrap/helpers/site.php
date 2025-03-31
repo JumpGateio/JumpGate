d09589486fac381b3a572e6defea5c804c653dac
Stygiansabyss <stygian.warlock.v2@gmail.com>
2025-03-30 23:59:40 -0500

    Adding dir helper again

--- bootstrap/helpers/site.php
+++ bootstrap/helpers/site.php
@@ -1,2 +1,12 @@
 <?php
 
+function dir_style($parts): string
+{
+    // This site is developed on windows but presented on linux.
+    $ending = '/';
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        $ending = '\\';
+    }
+
+    return implode($ending, $parts);
+}
