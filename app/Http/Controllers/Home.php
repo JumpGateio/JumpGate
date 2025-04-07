f0fce60c1ae40398f76b813dfe667f170f349019
Stygiansabyss <stygian.warlock.v2@gmail.com>
2025-04-07 00:02:46 -0500

    Adding test broadcasting code

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TestBroadcasting;
 
 class Home extends Base
 {
     public function index()
     {
         $loggedIn = auth()->check();
+
+        // Test Broadcasting
+        broadcast(new TestBroadcasting());
 
         return $this->response(
             compact('loggedIn'),
